87dee35674068279abe65bd48187160d18fa421e
Andrew Almaguer <andrew@almaguer.dev>
2019-05-03 20:25:39 +0200

    fix formating on session data

--- rest/admin.py
+++ rest/admin.py
@@ -3,9 +3,9 @@ import pprint
 from django.contrib.sessions.models import Session
 class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
-        return pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n')
+        return pprint.pformat(obj.get_decoded())
     _session_data.allow_tags=True
-    list_display = ['session_key', '_session_data', 'expire_date']
+    list_display = ['expire_date', 'session_key', '_session_data']
     readonly_fields = ['_session_data']
     exclude = ['session_data']
     date_hierarchy='expire_date'
