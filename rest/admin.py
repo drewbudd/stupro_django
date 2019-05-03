e8cab98274d7a22dfa9e2122143358c1ec63de6d
Andrew Almaguer <andrew@almaguer.dev>
2019-05-03 20:28:49 +0200

    make another small change to json parsing of session info

--- rest/admin.py
+++ rest/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-import pprint
 from django.contrib.sessions.models import Session
 class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
-        return pprint.pformat(obj.get_decoded())
+        return obj.get_decoded()
     _session_data.allow_tags=True
     list_display = ['expire_date', 'session_key', '_session_data']
     readonly_fields = ['_session_data']
