dcdd4e4e92e51ecd3d0eb0ca59a4eb27657aa735
Andrew Almaguer <andrew@almaguer.dev>
2019-05-03 09:28:14 +0200

    implement basic rest

--- rest/views.py
+++ rest/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from cadmodels.models import CADModel, Marker, Status, Type
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, CADModelSerializer, MarkerSerializer, StatusSerializer, TypeSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class CADModelViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = CADModel.objects.all()
+    serializer_class = CADModelSerializer
+
+class MarkerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Marker.objects.all()
+    serializer_class = MarkerSerializer
+
+class StatusViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+class TypeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
--- stuprosample/urls.py
+++ stuprosample/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rest import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserSerializer)
+router.register(r'groups', views.GroupSerializer)
+router.register(r'cadmodels', views.CADModelSerializer)
+router.register(r'markers', views.MarkerSerializer)
+router.register(r'statuses', views.StatusSerializer)
+router.register(r'types', views.TypeSerializer)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest/', include(router.url))
 ]
